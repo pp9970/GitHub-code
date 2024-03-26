c303bf22dbae901b2622bbf59b54e78a5d3bf665
pp9970 <pratikb171@gmail.com>
2024-03-26 19:27:11 +0530

    first commit for MavenProjectForJenkins

---
+++ src/main/java/Package1/Class1.java
@@ -0,0 +1,13 @@
+package Package1;
+
+public class Class1 {
+
+	public static void method1() {
+		System.out.println("method1");
+	}
+	
+	public static void main(String[] args) {
+		method1();
+		
+	}
+}
---
+++ src/main/java/Package1/Class2.java
@@ -0,0 +1,17 @@
+package Package1;
+
+public class Class2 {
+
+	public static void method2() {
+		System.out.println("method2");
+	}
+	
+	public static void method3() {
+		System.out.println("method3");
+	}
+	
+	public static void main(String[] args) {
+		method2();
+		method3();
+	}
+}
